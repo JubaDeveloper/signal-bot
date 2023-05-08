f1a1a1870edecdad58c97c1f2e0f6d71cc552024
jubadev <michaelres1332@gmail.com>
2023-05-07 21:14:11 -0300

    Message events is captured

---
+++ src/main/java/org/example/bot/SignalBot.java
@@ -0,0 +1,11 @@
+package org.example.bot;
+
+
+public class SignalBot {
+    public SignalBot () {}
+
+    public void onMessage (String message) {
+        System.out.println(message);
+    }
+
+}
--- src/main/java/org/example/socket/connection/listener/SBSocketListener.java
+++ src/main/java/org/example/socket/connection/listener/SBSocketListener.java
@@ -1,16 +1,23 @@
 package org.example.socket.connection.listener;
 
+import org.example.bot.SignalBot;
+
 import static java.lang.Thread.sleep;
 import java.net.http.WebSocket;
 import java.util.concurrent.CompletionStage;
 
 public class SBSocketListener implements WebSocket.Listener {
     StringBuilder message = new StringBuilder();
+    SignalBot signalBot;
+
+    public SBSocketListener () {
+        this.signalBot = new SignalBot();
+    }
     @Override
     public CompletionStage<?> onText(WebSocket webSocket, CharSequence data, boolean last) {
         if (last) {
             if (message.length() > 0) {
-                System.out.println(message);
+                this.signalBot.onMessage(message.toString());
             }
             message.replace(0, message.length(), "");
         } else {
